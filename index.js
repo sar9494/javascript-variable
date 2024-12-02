2d2e6922e95241778cf6793f162950cce81c10a1
sar9494 <batbayarsaruul0@gmail.com>
2024-12-02 18:29:03 +0800

    fyae

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+const a=3;
+const b=4;
+const c=5;
 
-const firstName="saruul" , nana="fyrit";
-const lastName="batbayar";
-const maritalStatus=false;
-const country="mongolia";
-const age=20;
-console.log(firstName , nana , lastName , maritalStatus , country , age);
+const P=a+b+c;
+const p=P/2;
+const s=3.14*a*a;
+const x1=10;
+const x2=10;
+const y1=10;
+const y2=10;
+const n=y1/x2;
 
+console.log("p=",p ,"s=",s ,p);
+console.log("m=",y2-n-x1);
+console.log(150/3);
+const sT=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+console.log(sT);
+const C=40;
+const F=C*9/5+32;
+console.log("F=",F);
+const aTeam=96+108+89;
+const bTeam=88+91+110;
+console.log(aTeam/3 , bTeam/3  );
+const q=15;
+const w=10;
+console.log((q+w)===25);
+console.log((q-w)===5);
+
+const a2=5
+console.log(a2/2.54);
+const radius=prompt("radius="," ")
+console.log("V=", Math.pow(radius,3) , "P=", 2*3.14*radius);
+
+const three=prompt("too oruulah","");
+console.log("ehnii oron", parseInt(three/100));
